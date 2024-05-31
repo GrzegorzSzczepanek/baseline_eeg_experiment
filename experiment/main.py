02b261fbdc5d9439c629ac791a472d9525ab8398
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2024-05-31 18:46:15 +0200

    last attempts of improving the data

--- experiment/main.py
+++ experiment/main.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     root = tk.Tk()
     participant_id = input("Enter participant ID: ")
     app = EEGExperiment(root=root, participant_id=participant_id)
-    # app.start()
 
     root.mainloop()
